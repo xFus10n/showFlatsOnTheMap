630e078bfd502a67886bf0d8d7f7b4f93a6eb056
rrazumilovs <razumilovrenat@inbox.lv>
2022-08-10 09:58:56 +0300

    encapsulation

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import stages.show_street as draw
 import stages.load_ss_lv as load
 import utilz.functions as f
 
-if __name__ == '__main__':
+
+def main():
     # first stage
     use_proxy = f.needs_proxy()
     load.use_proxy = use_proxy
     load.main()
-
     # second stage
     clean.main()
-
     # third stage
     draw.use_proxy = use_proxy
     draw.main()
+
+
+if __name__ == '__main__':
+    main()
